0574261452bb365771081743efe286482cdb220b
Jackie Dallas <jackie.r.dallas@gmail.com>
2023-04-29 00:43:04 -0400

    completed leetcode counter problem

---
+++ Javascript/counter.js
@@ -0,0 +1,20 @@
+// Given an integer n, return a counter function. This counter function initially returns n and then returns 1 more than the previous value every subsequent time it is called (n, n + 1, n + 2, etc).
+/** 
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
+
+// This code uses a closures: A closure is the combination of a function bundled together (enclosed) with references to its surrounding state (the lexical environment). In other words, a closure gives you access to an outer function's scope from an inner function. In JavaScript, closures are created every time a function is created, at function creation time.
+
+var createCounter = function(n) {
+    return function() {
+        console.log(n++);
+    };
+};
+
+const counter = createCounter(10)
+counter()
+counter()
+counter()
